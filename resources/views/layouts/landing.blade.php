f30a3bcb8f12b0d6b12701a3f48cead0b761e76f
samirph <samirphaddad@gmail.com>
2015-08-30 14:19:48 -0300

    Alterações finais na landing page

--- resources/views/layouts/landing.blade.php
+++ resources/views/layouts/landing.blade.php
@@ -6,7 +6,7 @@
 -->
 <html>
 	<head>
-		<title>MyHouse ON</title>
+		<title>My Home ON</title>
 		<meta charset="utf-8" />
 		<meta name="viewport" content="width=device-width, initial-scale=1" />
 		<!--[if lte IE 8]><script src="{{asset('js/ie/html5shiv.js')}})"></script><![endif]-->
@@ -17,32 +17,7 @@
 		<div id="page-wrapper">
 
 			<!-- Header -->
-				<header id="header" class="alt">
-					<h1><a href="index.html">Alpha</a> by HTML5 UP</h1>
-					<nav id="nav">
-						<ul>
-							<li><a href="index.html">Home</a></li>
-							<li>
-								<a href="#" class="icon fa-angle-down">Layouts</a>
-								<ul>
-									<li><a href="generic.html">Generic</a></li>
-									<li><a href="contact.html">Contact</a></li>
-									<li><a href="elements.html">Elements</a></li>
-									<li>
-										<a href="#">Submenu</a>
-										<ul>
-											<li><a href="#">Option One</a></li>
-											<li><a href="#">Option Two</a></li>
-											<li><a href="#">Option Three</a></li>
-											<li><a href="#">Option Four</a></li>
-										</ul>
-									</li>
-								</ul>
-							</li>
-							<li><a href="#" class="button">Sign Up</a></li>
-						</ul>
-					</nav>
-				</header>
+				
 
 			<!-- Banner -->
 				<section id="banner">
@@ -54,7 +29,7 @@
 						<li><a href="#" class="button">Veja mais</a></li>
 					</ul>
                     -->
-                    <img src="{{asset('images/logo-small.jpg')}}">
+                    <img src="{{asset('images/logo-novo.jpg')}}">
 				</section>
 
 			<!-- Main -->
@@ -62,13 +37,12 @@
 
 					<section class="box special">
 						<header class="major">
-							<h2>Apresentando a nossa solução
-							<br />
-							para a falta de controle do seu consumo de energia</h2>
+							<h2>A sofisticação que a sua casa precisava!
+							</h2>
 							<p>Sua casa sempre ao alcance das suas mãos<br />
 							</p>
 						</header>
-						<span class="image featured"><img src="images/pic01.jpg" alt="" /></span>
+						<span class="image featured"><img src="images/screenshot.png" alt="" /></span>
 					</section>
 
 					<section class="box special features">
