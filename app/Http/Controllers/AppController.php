c7aef0edb8d77feedeee6a48b3bb5cbe176e2826
samirph <samirphaddad@gmail.com>
2015-08-30 10:40:08 -0300

    bug arrumado no appcontroller

--- app/Http/Controllers/AppController.php
+++ app/Http/Controllers/AppController.php
@@ -117,7 +117,7 @@ class AppController extends Controller
         $st = DB::table('onoff')->select('status')->orderBy('created_at','desc')->first();
         
         $status = 'checked';
-        if($st->status < 1 ){
+        if(isset($st->status)&&$st->status <1 ){
             $status = '';
         }
         return view('app/index',compact('status'));
